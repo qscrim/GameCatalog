6784cdf3e4ff04268b8737cb058bc15f8fe7e5a0
qscrim <tochnonemaltsev@yandex.ru>
2026-05-18 21:27:12 +0700

    Add splash screen, release theme & proguard rules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,4 +86,5 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
--- app/src/main/java/com/example/gamecatalog/MainActivity.kt
+++ app/src/main/java/com/example/gamecatalog/MainActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.gamecatalog.ui.theme.GameCatalogTheme
 import com.example.gamecatalog.presentation.screens.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Сплэш-скрин должен быть установлен ДО super.onCreate()
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
